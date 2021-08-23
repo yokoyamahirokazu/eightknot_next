f650000dcbcd1f252d4e80d33a075f539538e54b
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-08-23 18:57:31 +0900

    first commit

---
+++ pages/components/content.js
@@ -0,0 +1,14 @@
+import Header from './header'
+
+export default function Content({ children }) {
+    return (
+      <>
+      <Header></Header>
+      <div className="container">
+        { children }
+       </div>
+       <footer>©︎ Eight Knot Inc.
+       </footer>
+       </>
+    )
+}
---
+++ pages/components/header.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function Header(props) {
+    return (
+
+        <div>
+            <p>Eight Knot</p>
+    <Link href={`/`}><a>トップへ</a></Link>
+    <Link href={`/about`}><a>About</a></Link>
+</div>
+
+
+    )
+}
