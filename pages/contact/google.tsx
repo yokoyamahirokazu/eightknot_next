6c38d41e4a7348dff4dffb92e52488d3eb27299b
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-08-31 12:26:40 +0900

    axios cors2

--- pages/contact/google.tsx
+++ pages/contact/google.tsx
@@ -217,7 +217,6 @@ const submit = (values) => {
   submitParams.append(ContactGoogleForm.typeother, values.typeother);
   submitParams.append(ContactGoogleForm.body, values.body);
 
-  axios.defaults.baseURL = 'https://eightknot-next.vercel.app/';
   axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
