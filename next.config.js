6e931150228b8e89113e75a0a8e1826b731caf69
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-08-31 21:00:39 +0900

    ios対応

--- next.config.js
+++ next.config.js
@@ -23,7 +23,8 @@ module.exports = {
         source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'http://*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
